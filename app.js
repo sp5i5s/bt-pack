c9367394211ebb1d6b94e1bd7b3ee58fb8988cac
brandon fang <brandonfang@userdeMacBook-Pro.local>
2019-01-07 23:20:44 +0800

    update

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 const utils = require('./system/utils');
+const watch = require('watch');
+
 module.exports = {
     run(){
         utils.queryFolderFiles();
+        watch.watchTree(global.listenDirectory, function (f, curr, prev) {
+            bt.log('文件监听中' + new Date().toLocaleString());
+            if (typeof f == "object" && prev === null && curr === null) {
+            }else{
+                bt.log('正在重新打包 - ' + new Date().toLocaleString());
+                utils.queryFolderFiles();
+                bt.log('打包完成 - ' + new Date().toLocaleString());
+            }
+        })
     }
 };
